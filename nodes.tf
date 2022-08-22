46238b40c33466e4a06a30ad04e963c965291e42
Jakub Sokołowski <jakub@status.im>
2022-08-22 16:11:52 +0200

    status: enable native websocket port
    https://github.com/status-im/infra-status/issues/13
    
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- nodes.tf
+++ nodes.tf
@@ -16,6 +16,12 @@ module "hosts" {
   gc_type = local.ws["gc_type"] /* Google Cloud */
 
   /* firewall */
-  open_tcp_ports = [ "30303" ] /* p2p main */
-  open_udp_ports = [ "9000" ]  /* discovery v5 */
+  open_tcp_ports = [
+    "30303", /* p2p main */
+    "443",   /* websocket */
+    "80",    /* certbot */
+  ]
+  open_udp_ports = [
+    "9000",  /* discovery v5 */
+  ]
 }
