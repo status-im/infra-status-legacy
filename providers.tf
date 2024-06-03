844a5e417ff6ea2cf69fb0591a98fad2de8a415f
Jakub Sokołowski <jakub@status.im>
2024-06-03 17:10:06 +0200

    providers.tf: drop unused account_id key
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- providers.tf
+++ providers.tf
@@ -7,7 +7,6 @@ provider "digitalocean" {
 provider "cloudflare" {
   email      = data.pass_password.cloudflare_email.password
   api_key    = data.pass_password.cloudflare_token.password
-  account_id = data.pass_password.cloudflare_account.password
 }
 
 provider "google" {
